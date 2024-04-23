91e91725d0cacefd53310c3e387f94a663da59c8
Judy <devzoou@gmail.com>
2024-04-23 18:48:08 +0900

    feat: snackBar 표시 공통 함수 추가

--- lib/src/constants/utils.dart
+++ lib/src/constants/utils.dart
@@ -3,3 +3,13 @@ import 'package:flutter/material.dart';
 /// 다크모드 여부
 bool isDarkMode(BuildContext context) =>
     MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+/// snackbar 표시
+void showSnackbar(BuildContext context, String txt) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(txt),
+      showCloseIcon: true,
+    ),
+  );
+}
